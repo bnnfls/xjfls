19b31789b4aebb8398bed2f0f1550596f35348b2
bnnfls <61794600+bnnfls@users.noreply.github.com>
2020-03-06 11:14:48 +0800

    Create domains.js

---
+++ domains.js
@@ -0,0 +1,9 @@
+if (domains = 'undefined') {
+    domains = [];
+}
+
+if (domains != 'undefined') {
+    domains.push('xjfls.xyz');
+    domains.push('bfls888.club');
+    domains.push('flsbnn.club');
+}
