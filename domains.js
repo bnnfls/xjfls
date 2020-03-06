e90bb145292e513e2e569424337e283a309baf0a
bnnfls <61794600+bnnfls@users.noreply.github.com>
2020-03-06 12:19:34 +0800

    Update domains.js

--- domains.js
+++ domains.js
@@ -2,8 +2,6 @@ if (domains == 'undefined') {
     domains = [];
 }
 
-if (domains != 'undefined') {
-    domains.push('xjfls.xyz');
-    domains.push('bfls888.club');
-    domains.push('flsbnn.club');
-}
+domains.push('xjfls.xyz');
+domains.push('bfls888.club');
+domains.push('flsbnn.club');
