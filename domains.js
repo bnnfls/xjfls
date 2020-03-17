233568171c9298012afb3f34c25d02a87a00a482
bnnfls <61794600+bnnfls@users.noreply.github.com>
2020-03-17 15:14:38 +0800

    Update domains.js

--- domains.js
+++ domains.js
@@ -5,3 +5,16 @@ if (domains == 'undefined') {
 domains.push('xjfls.xyz');
 domains.push('bfls888.club');
 domains.push('flsbnn.club');
+domains.push('69sp.xyz');
+domains.push('91gcsp.xyz');
+domains.push('aibbzx.xyz');
+domains.push('douyinzx.xyz');
+domains.push('fanqiesp.xyz');
+domains.push('fedtp.xyz');
+domains.push('fls188688866.xyz');
+domains.push('kedouwjp.xyz');
+domains.push('lulushesp.xyz');
+domains.push('sszx1.xyz');
+domains.push('x333333.xyz');
+domains.push('xv8868.xyz');
+domains.push('yoyosp.xyz');
